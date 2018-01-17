a3844aea6050bca1ef1d1ce72e3e12de471d8c8e
wangdongxu <18201451992@163.com>
2018-01-17 20:44:07 +0800

    aab

--- src/com/java/controller/HelloController.java
+++ src/com/java/controller/HelloController.java
@@ -1,10 +1,23 @@
 package com.java.controller;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
 public class HelloController {
-	
+	@RequestMapping("/test")
+	@ResponseBody
+	public Map<String, Object> name(HttpServletRequest paramsMap) throws Exception{
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		return map ;
+	}
 }
